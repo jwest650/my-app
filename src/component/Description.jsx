f97ac53dd9a8c4f49a87037453eb3b04464421fe
jwest <jwest650gmail.com>
2021-12-03 11:17:34 -0800

    first commit

---
+++ src/component/Description.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Description = ({ description }) => {
+    return (
+        <div className="desc">
+            <p>{description}</p>
+        </div>
+    );
+};
+
+export default Description;
